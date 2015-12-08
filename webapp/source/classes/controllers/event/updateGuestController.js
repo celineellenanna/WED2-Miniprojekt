19d8631dd1cd854cf0ee863992aa6fb6c233be66
rustaska <sdefatsc@hsr.ch>
2015-12-08 16:13:54 +0100

    edited controller file

--- webapp/source/classes/controllers/event/updateGuestController.js
+++ webapp/source/classes/controllers/event/updateGuestController.js
@@ -1,33 +1,28 @@
 /**
  * Created by Céline on 03.12.2015.
  */
-define(['app/model/event'], function(Event) {
+define(['app/model/guest'], function(Guest) {
     'use strict';
 
     var GuestUpdateController = function($scope, $location, EventRepository, $routeParams){
-        this.scope = $scope;
-        this.scope.event = new Event();
-        var eventId = $routeParams.eventId;
 
+        $scope.guest = new Guest();
 
-        EventRepository.get(
-            { id:$routeParams.eventId },
-            function(event) {
-                this.scope.event = event;
-                this.scope.event.times.begin = new Date(event.times.begin);
-                this.scope.event.times.end = new Date(event.times.end);
-            }.bind(this),
-            function() {}
+        EventRepository.getGuest($routeParams.eventId, $routeParams.guestId,
+        function(guest){
+        console.log(guest);
+        $scope.guest = guest;
+        },function(){}
         );
 
-        this.scope.update = function(event){
 
-            $scope.event = event;
-            EventRepository.update(eventId, event, function(event) {
-                $location.path('/events/'+event.id);
+
+        $scope.update = function(){ console.log($scope.guest);
+            EventRepository.updateGuest($routeParams.eventId, $routeParams.guestId, $scope.guest, function() {
+                $location.path('/events/'+$routeParams.eventId);
             }, function(){});
 
-        }
+        };
     };
 
     GuestUpdateController.$inject = ['$scope', '$location', 'EventRepository', '$routeParams'];
